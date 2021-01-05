38593de50afd429a777c8b14a6ad9035294f2042
lemon <lemon@lemon-Lenovo-B50-70>
2021-01-05 11:04:35 +0100

    1- Estructura creada y modificación del README

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/MainApp.java
@@ -0,0 +1,10 @@
+package org.iesalandalus.programacion.cuatroenraya;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
